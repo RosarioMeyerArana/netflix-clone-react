3e43d28904f1eb4ff3eb896afb92723749f6b25d
rosario meyer arana <rosario.meyera@gmail.com>
2021-07-28 12:14:21 +0200

    banner and layout

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = 'd59f74438cab9389b251e2ee87ac5972'
+
+const requests = {
+    fetchTrending:`trending/all/day?api_key=${API_KEY}&language=en_US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`
+
+}
+
+export default requests
